d0cb65fcb513ddb8143bd20c75d0730d4fccdc1f
khmaladze-tiko <73258377+khmaladze-tiko@users.noreply.github.com>
2024-01-23 18:55:19 +0400

    the sideDrawer

--- src/components/header_footer/Header.js
+++ src/components/header_footer/Header.js
@@ -1,14 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import { AppBar, Toolbar, IconButton } from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
 import SideDrawer from "./SideDrawer";
 
 const Header = () => {
+    const [drawerOpen, setDrawerOpen] = useState(false)
+
+    const toggleDrawer = (value) => {
+        setDrawerOpen(value)
+    }
+
+
+
   return (
     <AppBar
       position="fixed"
       style={{
-        backgroundColor: "2f2f2f",
+        backgroundColor: "#2f2f2f",
         boxShadow: "none",
         padding: "10px 0px",
       }}
@@ -19,10 +27,17 @@ const Header = () => {
           <div className="header_logo_title">Musical Events</div>
         </div>
 
-        <IconButton aria-label="Menu" color="inherit">
+        <IconButton 
+            aria-label="Menu"
+            color="inherit"
+            onClick={()=> toggleDrawer(true)}
+        >
           <MenuIcon />
         </IconButton>
-        <SideDrawer />
+        <SideDrawer 
+            open={drawerOpen}
+            onClose={(value)=>toggleDrawer(value)}
+        />
       </Toolbar>
     </AppBar>
   );
