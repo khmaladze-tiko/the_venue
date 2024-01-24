f9299fa63db056d5a788140625f91c9fff8905ac
khmaladze-tiko <73258377+khmaladze-tiko@users.noreply.github.com>
2024-01-24 20:13:14 +0400

    toggling header state

--- src/App.js
+++ src/App.js
@@ -7,6 +7,11 @@ const App = () => {
   return (
     <div className="App">
       <Header/>
+      <div style={{backgroundColor: 'red', height: '800px'}}></div>
+      <div style={{backgroundColor: 'blue', height: '800px'}}></div>
+      <div style={{backgroundColor: 'green', height: '800px'}}></div>
+      <div style={{backgroundColor: 'yellow', height: '800px'}}></div>
+
     </div>
   );
 }
--- src/components/header_footer/Header.js
+++ src/components/header_footer/Header.js
@@ -1,22 +1,31 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { AppBar, Toolbar, IconButton } from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
 import SideDrawer from "./SideDrawer";
 
 const Header = () => {
     const [drawerOpen, setDrawerOpen] = useState(false)
-
+    const [headerShow, setHeaderShow] = useState(false)
+    const handleScroll = () => {
+        if(window.scrollY > 0) {
+            setHeaderShow(true)
+        } else {
+            setHeaderShow(false)
+        }
+    }
     const toggleDrawer = (value) => {
         setDrawerOpen(value)
     }
 
-
+    useEffect(() => {
+        window.addEventListener('scroll', handleScroll)
+    }, [])
 
   return (
     <AppBar
       position="fixed"
       style={{
-        backgroundColor: "#2f2f2f",
+        backgroundColor: headerShow ? "#2f2f2f" : 'transparent',
         boxShadow: "none",
         padding: "10px 0px",
       }}
