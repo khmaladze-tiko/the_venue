6112ebd94391112ae5bfc61982af34108ad81488
khmaladze-tiko <73258377+khmaladze-tiko@users.noreply.github.com>
2024-02-01 01:51:13 +0400

    finishing the countdown

--- src/components/featured/timeUntil.js
+++ src/components/featured/timeUntil.js
@@ -3,6 +3,13 @@ import { Slide } from "react-awesome-reveal";
 
 const TimeUntil = () => {
 
+    const [time, setTime] = useState({
+        days:'0',
+        hours:'0',
+        minutes:'0',
+        seconds:'0'
+    })
+
     const renderItem = (time,value) => (
         <div className="countdown_item">
             <div className="countdown_time">
@@ -14,18 +21,28 @@ const TimeUntil = () => {
         </div>
     )
 
-        const getTimeUntil = (deadline) => {
+        const getTimeUntil = useCallback( (deadline) => {
             const time = Date.parse(deadline) - Date.parse(new Date());
 
             if(time < 0){
                 console.log('Date passed')
             }else{
-                console.log('still going on')
+               const seconds = Math.floor((time/1000)%60);
+               const minutes = Math.floor((time/1000/60)%60);
+               const hours = Math.floor((time/(1000*60*60))%24);
+               const days = Math.floor((time/(1000*60*60*24)));
+               
+               setTime({
+                days,
+                hours,
+                minutes,
+                seconds
+               })
             }
-        }
+        }, [])
 
         useEffect(() => {
-            setInterval(() => getTimeUntil('Jan, 20, 2021, 01:20:00'),1000)
+            setInterval(() => getTimeUntil('Dec, 20, 2024, 01:20:00'),1000)
         },[getTimeUntil])
 
 
@@ -36,10 +53,10 @@ const TimeUntil = () => {
                     Event starts in
                 </div>
                 <div className="countdown_bottom">
-                       {renderItem(27,'Days')}
-                       {renderItem(4,'HS')}
-                       {renderItem(10,'Min')}
-                       {renderItem(27,'Sec')}
+                       {renderItem(time.days,'Days')}
+                       {renderItem(time.hours,'HS')}
+                       {renderItem(time.minutes,'Min')}
+                       {renderItem(time.seconds,'Sec')}
                 </div>
             </div>
         </Slide>
